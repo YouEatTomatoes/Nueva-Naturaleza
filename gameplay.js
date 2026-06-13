2fca6a84b88b2d21371ea0a5dd2b714c40865907
YouEatTomatoes <503theophilus@gmail.com>
2026-06-12 22:49:42 -0700

    stabilize and polish first stage of site

--- gameplay.js
+++ gameplay.js
@@ -3,24 +3,20 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 var mobile = false;
 
-function resizeTheGame() {
-    if (/Android|Mobi|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/.test(navigator.userAgent)) {
-        canvas.width = "400";
-        canvas.height = "600"; 
-        mobile = true;
-    } else {
-        canvas.width = "400";
-        canvas.height = "400"; 
-        mobile = false;
-        if (window.innerWidth > window.innerHeight) {
-            console.log("ITS TOO BIG!!");
-            //frameofgame.style.maxWidth = "${window.innerHeight}px";
-            //frameofgame.style.width = "auto";
-        }
-    }
-    ctx.fillStyle = "blue";
-    ctx.fillRect(100, 100, 200, 200);
+if (/Android|Mobi|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/.test(navigator.userAgent)) {
+    canvas.width = "400";
+    canvas.height = "600"; 
+    mobile = true;
+} else {
+    canvas.width = "400";
+    canvas.height = "400"; 
+    mobile = false;
 }
-resizeTheGame();
-window.addEventListener("resize", resizeTheGame);
-
+ctx.fillStyle = "gray";
+ctx.fillRect(0, 0, 400, 400);
+ctx.fillStyle = "blue";
+ctx.fillRect(100, 100, 200, 200);
+if (mobile) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 400, 400, 200)
+}

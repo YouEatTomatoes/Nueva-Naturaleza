b9acb09827b7b1e5e1c791b31ddd6ced19c92360
YouEatTomatoes <503theophilus@gmail.com>
2026-06-22 23:23:21 -0700

    add image loading and displaying

--- gameplay.js
+++ gameplay.js
@@ -2,21 +2,38 @@ const frameofgame = document.getElementById("gameframe");
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 var mobile = false;
+var phase = -1;
 
 if (/Android|Mobi|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/.test(navigator.userAgent)) {
-    canvas.width = "400";
-    canvas.height = "600"; 
+    canvas.width = "256";
+    canvas.height = "384"; 
     mobile = true;
 } else {
-    canvas.width = "400";
-    canvas.height = "400"; 
+    canvas.width = "256";
+    canvas.height = "256"; 
     mobile = false;
 }
-ctx.fillStyle = "gray";
-ctx.fillRect(0, 0, 400, 400);
-ctx.fillStyle = "blue";
-ctx.fillRect(100, 100, 200, 200);
-if (mobile) {
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 400, 400, 200)
+
+const asset = {
+    start1: new Image()
+    //start2: new Image()
+};
+var loadTracker = Object.keys(asset).length * -1;
+asset.start1.onload = function() {loadTracker += 1;}
+//asset.somethin.onload = function() {loadTracker += 1;}
+asset.start1.src = "./Assets/naturastartscreen.png"
+
+
+function draw() {
+    requestAnimationFrame(draw);
+    if (phase === -1) {
+        console.log("help im still loading :O");
+        if (loadTracker === 0) {
+            phase = 0
+        }
+    } else if (phase === 0) {
+        console.log("yay we loaded!!!")
+        ctx.drawImage(asset.start1, 0, 0);
+    }
 }
+draw();
